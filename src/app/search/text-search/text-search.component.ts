881072b51b59f4ae77ad1df3ce609868e00075a6
strikertanmay <tanmaymittal0@gmail.com>
2019-03-30 23:23:04 +0530

    axios added

--- src/app/search/text-search/text-search.component.ts
+++ src/app/search/text-search/text-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MedicineShort} from '../../medicine-short.model';
+import { MedicineShort } from '../../medicine-short.model';
+import axios from 'axios';
 
 @Component({
   selector: 'app-text-search',
@@ -13,11 +14,30 @@ export class TextSearchComponent implements OnInit {
   medicines: MedicineShort[] = [];
 
   ngOnInit() {
+    this.getMedicines()
   }
 
   getMedicines() {
+    axios.get('http://127.0.0.1:5000/medicine/oflox/', {
+      headers:
+        { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET' }
+    }).then(res => {
+      console.log(res.data);
+      res.data.forEach((temp: MedicineShort) => {
 
+        const newObject: MedicineShort = {
+          name: temp.name,
+          link: temp.name,
+          price: temp.price
+        }
+        console.log(newObject);
 
+      })
+
+    })
+      .catch(err => {
+        console.log(err);
+      });
 
 
   }
